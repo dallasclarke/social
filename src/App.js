52a72172d508613b3eea40ebb90f19d1c656bcec
Dallas <dallas.clarke622@gmail.com>
2021-04-19 16:35:28 -0700

    Starting react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import {Container} from "react-bootstrap"
+import { Container } from "react-bootstrap";
 import "./styles/App.scss";
 
 import Login from "./components/login/Login";
@@ -9,13 +10,18 @@ import UserProfile from "./components/profile/UserProfile";
 
 function App() {
   return (
-    <div className="App">
-      <Container fluid>
-        {/* <Login /> */}
-        {/* <Feed /> */}
-        <UserProfile />
-      </Container>
-    </div>
+    <Router>
+      <div className="App">
+        <Container fluid>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/feed" component={Feed} />
+          <Route exact path="/profile" component={UserProfile} />
+          {/* <Login /> */}
+          {/* <Feed /> */}
+          {/* <UserProfile /> */}
+        </Container>
+      </div>
+    </Router>
   );
 }
 
